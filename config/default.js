ebd7196c2c0fd7ed4093b051553002429e268d5d
Gavin Shaw <gavshaw+github@gmail.com>
2024-06-22 15:14:11 -0400

    Add config file and set a capacity for the cache
    - Basic cache capacity and eviction, where a random record is selected
      for removal to make room for the new record.

---
+++ config/default.js
@@ -0,0 +1,7 @@
+const config = {
+    cache: {
+        capacity: 10,
+    },
+};
+
+module.exports = config;
--- service/ip_to_country_service.js
+++ service/ip_to_country_service.js
@@ -1,5 +1,9 @@
+const config = require('config');
+
 const IPCountryModel = require('../model/IPCountryModel');
 const ipgeolocation = require('../vendors/ipgeolocation/convert')
+
+const cacheConfig = config.get('cache');
 
 const cache = new Map();
 
@@ -11,6 +15,25 @@ exports.convertIPToCountry = async function (ip) {
 
     // IP not in cache, so ask vendor to convert and add result to cache
     let country = await ipgeolocation.convert(ip);
-    cache.set(ip, country);
+    insertIntoCache(ip, country)
     return new IPCountryModel(ip, country);
+}
+
+insertIntoCache = function (ip, country) {
+    // Check if the cache is at capacity
+    if (cache.size < cacheConfig.capacity) {
+        // Cache is not at capacity, so add
+        cache.set(ip, country);
+    } else {
+        // Cache is at capacity, so randomly remove a record to make room
+        const ran = Math.floor(Math.random() * cacheConfig.capacity);
+        const iterator = cache.keys();
+        let ipToRemove;
+
+        for (let j = 0; j <= ran; j++) {
+            ipToRemove = iterator.next().value;
+        }
+        cache.delete(ipToRemove);
+        cache.set(ip, country);
+    }
 }
