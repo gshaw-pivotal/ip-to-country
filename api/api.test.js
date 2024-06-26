ef9b7115423df6580c0ae51f795561e9641615e0
Gavin Shaw <gavshaw+github@gmail.com>
2024-06-25 21:01:06 -0400

    Improve unit test coverage for api.js

--- api/api.test.js
+++ api/api.test.js
@@ -2,7 +2,12 @@ const assert = require("node:assert");
 const { describe, it } = require("node:test");
 const {mock} = require("cjs-mock");
 const request = require("supertest");
+
 const {app} = require("./api");
+const IPCountryModel = require("../model/ip_to_country_model");
+const RateLimitExceededError = require("../error/rate_limit_exceeded_error");
+const ServiceError = require("../error/service_error");
+const VendorError = require("../error/vendor_error");
 
 describe('api', () => {
     it('should return bad request when there is no request body', async () => {
@@ -30,5 +35,68 @@ describe('api', () => {
             .post('/country')
             .send({ip: 'not-an-ip-address'})
             .expect(400);
+    });
+
+    it('should return a 429 when ip_to_country_service throws a RateLimitExceededError', async () => {
+        const mocked = mock('./api', {
+            '../service/ip_to_country_service': {
+                convertIPToCountry: (ip) => {
+                    throw new RateLimitExceededError('Error')
+                },
+            },
+        });
+
+        await request(mocked.app)
+            .post('/country')
+            .send({ip: '1.1.1.1'})
+            .expect(429);
+    });
+
+    it('should return a 500 when ip_to_country_service throws a VendorError', async () => {
+        const mocked = mock('./api', {
+            '../service/ip_to_country_service': {
+                convertIPToCountry: (ip) => {
+                    throw new VendorError('Error')
+                },
+            },
+        });
+
+        await request(mocked.app)
+            .post('/country')
+            .send({ip: '1.1.1.1'})
+            .expect(500);
+    });
+
+    it('should return a 500 when ip_to_country_service throws a ServiceError', async () => {
+        const mocked = mock('./api', {
+            '../service/ip_to_country_service': {
+                convertIPToCountry: (ip) => {
+                    throw new ServiceError('Error')
+                },
+            },
+        });
+
+        await request(mocked.app)
+            .post('/country')
+            .send({ip: '1.1.1.1'})
+            .expect(500);
+    });
+
+    it('should return a 200 when ip_to_country_service returns successfully', async () => {
+        const result = new IPCountryModel('1.1.1.1', 'Country (Country-Code)')
+
+        const mocked = mock('./api', {
+            '../service/ip_to_country_service': {
+                convertIPToCountry: (ip) => Promise.resolve(result)
+            },
+        });
+
+        const response = await request(mocked.app)
+            .post('/country')
+            .send({ip: '1.1.1.1'});
+
+        assert.equal(response.status, 200);
+        assert.equal(response.body.ip, '1.1.1.1');
+        assert.equal(response.body.country, 'Country (Country-Code)');
     });
 });
